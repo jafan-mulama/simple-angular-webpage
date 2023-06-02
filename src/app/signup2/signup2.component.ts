34bd3f154baeacf47120c112ca1f5232233ff2d0
jafan-mulama <jafan2222@gmail.com>
2023-06-02 16:38:36 +0300

    added bidded (ngSubmit)='submitForm()

--- src/app/signup2/signup2.component.ts
+++ src/app/signup2/signup2.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-signup2',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup2.component.css']
 })
 export class Signup2Component {
+  form: FormGroup;
 
+  constructor() {
+    this.form = new FormGroup({
+      name: new FormControl(),
+      email: new FormControl(),
+      password: new FormControl()
+    });
+  }
+
+  submitForm() {
+    if (this.form.valid) {
+      // Perform form submission or further actions here
+      console.log(this.form.value);
+    } else {
+      // Handle form validation errors
+      // You can display validation errors to the user or perform any other required actions
+      console.log('Form is invalid');
+    }
+  }
 }
